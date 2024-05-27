c8bf8ca1c1ffc592c55dd8adba29f0c922364615
Barshon <barshonweb@gmail.com>
2024-05-27 12:32:48 +0600

    dashboard layout

--- src/Pages/Dashboard/Dashboard.jsx
+++ src/Pages/Dashboard/Dashboard.jsx
@@ -6,8 +6,8 @@ const Dashboard = () => {
           Welcome to the Dashboard
         </h1>
       </div>
-      <div className="flex justify-between mt-5">
-        <div className="ml-1 min-h-96 min-w-64 rounded text-white bg-blue-600">
+      <div className="flex flex-col md:flex-row justify-between mt-5">
+        <div className="mx-1 min-h-96 min-w-64 rounded text-white bg-blue-600">
             <div className="mt-2 text-center">
                 <p className="text-lg">Dashboard Options</p>
             </div>
