9dc1e90a6e44add41a0e62561e725c1d851d2859
Barshon <barshonweb@gmail.com>
2024-05-27 22:49:48 +0600

    json server setup

--- src/components/Dashboard/Menu.jsx
+++ src/components/Dashboard/Menu.jsx
@@ -3,11 +3,11 @@ import UserInfo from './UserInfo';
 
 const Menu = () => {
     return (
-        <div className="flex justify-between flex-col-reverse md:flex-row">
+        <div className="flex justify-between flex-col md:flex-row">
         <div className="">
           <div className=" min-h-72 md:min-h-screen min-w-64 rounded text-white bg-blue-600">
             <div className=" text-center">
-              <p className="text-lg">Dashboard Options</p>
+              <p className="text-lg pt-2">Dashboard Options</p>
               <div className='my-10 flex flex-col space-y-10'>
               <Link to={'/dashboardlayout/home'}><button className='bg-white px-16 py-2 rounded-lg text-blue-800 font-semibold'>Dashboard</button></Link>
               <Link to={'/dashboardlayout/allproducts'}><button className='bg-white px-16 py-2 rounded-lg text-blue-800 font-semibold'>All Products</button></Link>
