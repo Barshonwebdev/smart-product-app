8e58fc0354426b025078e09dee2d8aaee98599ab
Barshon <barshonweb@gmail.com>
2024-05-28 02:45:35 +0600

    explore page all products shown and responsive

--- src/components/Home/Banner.jsx
+++ src/components/Home/Banner.jsx
@@ -7,7 +7,7 @@ const Banner = () => {
         <div className="hero-content text-center text-neutral-content">
           <div className="max-w-xl ">
             <h1 className="mb-5 text-5xl font-bold">Welcome to Smart Product</h1>
-            <p className="mb-5">Choose products and gadgets of your liking, get delivered at your doorstep!</p>
+            <p className="mb-5 text-xl">Choose products and gadgets of your liking, get delivered at your doorstep!</p>
             <button className="btn btn-info">Begin Tour</button>
           </div>
         </div>
