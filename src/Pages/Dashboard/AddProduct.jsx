4e96ae10ed5713b4161320a7ca32017e2eb2cc84
Barshon <barshonweb@gmail.com>
2024-05-28 15:08:53 +0600

    add a product form design responsive

--- src/Pages/Dashboard/AddProduct.jsx
+++ src/Pages/Dashboard/AddProduct.jsx
@@ -1,11 +1,111 @@
-import React from 'react';
+import React from "react";
 
 const AddProduct = () => {
-    return (
-        <div>
-            Add Product
+  return (
+    <div>
+      <h1 className="my-5 text-2xl text-gray-500 text-center"> Add a product</h1>
+      <div className="hero  ">
+        <div className="hero-content flex-col lg:flex-row-reverse">
+          <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+            <form className="card-body grid grid-cols-2 gap-x-10">
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">ID</span>
+                </label>
+                <input
+                  type="text"
+                  placeholder="id"
+                  className="input input-bordered"
+                  required
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Name</span>
+                </label>
+                <input
+                  type="text"
+                  placeholder="name"
+                  className="input input-bordered"
+                  required
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Brand</span>
+                </label>
+                <input
+                  type="text"
+                  placeholder="brand"
+                  className="input input-bordered"
+                  required
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Price</span>
+                </label>
+                <input
+                  type="number"
+                  placeholder="price"
+                  className="input input-bordered"
+                  required
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Description</span>
+                </label>
+                <input
+                  type="text"
+                  placeholder="description"
+                  className="input input-bordered"
+                  required
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Availability</span>
+                </label>
+                <input
+                  type="text"
+                  placeholder="yes/no"
+                  className="input input-bordered"
+                  required
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Photo URL</span>
+                </label>
+                <input
+                  type="text"
+                  placeholder="photo url"
+                  className="input input-bordered"
+                  required
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Details</span>
+                </label>
+                <input
+                  type="text"
+                  placeholder="details"
+                  className="input input-bordered"
+                  required
+                />
+              </div>
+              
+              <div className="form-control mt-6 col-span-2">
+                <button className="btn text-white bg-blue-700">Add Product</button>
+              </div>
+            </form>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default AddProduct;
